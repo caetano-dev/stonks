a55ea85a1edea6934e3c5946ecef51ff219d2274
drull1000 <55260356+drull1000@users.noreply.github.com>
2019-12-16 14:46:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+import discord 
 import asyncio
 from discord.ext import commands
 import urllib.request as req
@@ -17,7 +17,7 @@ class Cotacao:
             return 0
 
     def dolar(self):
-        return self.__get_cotacao('http://dolarhoje.com/')
+        return (self.__get_cotacao('http://dolarhoje.com/'))
 
     def euro(self):
         return self.__get_cotacao('http://eurohoje.com/')
@@ -34,60 +34,82 @@ class Cotacao:
     def bitcoin(self):
         return self.__get_cotacao('https://dolarhoje.com/bitcoin-hoje/')
 
-cotacao = Cotacao()
-#-----------------------------------------------------------------------------------------------------------------------
-mensagem_bonitinha = ('Analisando minha bolsa de valores:chart_with_upwards_trend:...Por favor aguarde.')
 
-client = commands.Bot(command_prefix = '!')
+cotacao = Cotacao()
+
+dolar_float = float(cotacao.dolar())
+#-----------------------------------------------------------------------------------------------------------------------
+mensagem_bonitinha = ('Analisando minha bolsa de valores:chart_with_upwards_trend:...Por favor aguarde.') #mensagem antes dos valores
+
+client = commands.Bot(command_prefix = ".")
+
+client.remove_command('help')
 
 print('software online')
+
 @client.event
 async def on_ready():
     print('bot online')
 
 #----------------    #comandos
+@client.command(pass_context = True)
+async def help(ctx):
+    author = ctx.message.author        
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+    embed.set_author(name='Stonks')
+    embed.add_field(name="!dolar", value='Informa a cotação do Dolar.', inline = False)
+    embed.add_field(name="!euro", value='Informa a cotação do Euro.', inline = False)
+    embed.add_field(name="!rublo", value='Informa a cotação do Rublo.', inline = False)
+    embed.add_field(name="!bitcoin", value='Informa a cotação do Bitcoin.', inline = False)       #help
+    embed.add_field(name="!peso", value='Informa a cotação do Peso Argentino.', inline = False)
+    embed.add_field(name="!libra", value='Informa a cotação da libra.', inline = False)
+    embed.add_field(name="!converter", value='converte um valor em dolar para real.', inline = False)
+    await ctx.send(author, embed=embed)
 
-@client.command()
+
+
+@client.command()                          
 async def dolar(ctx):
     await ctx.send(mensagem_bonitinha)     
-    await ctx.send(f'1 Dolar está custando {cotacao.dolar()} reais :money_with_wings: ')    #dolar
+    await ctx.send(f'1 Dolar está custando {cotacao.dolar()} reais. :money_with_wings: ')    #dolar
 
 
 @client.command()                     
 async def euro(ctx):
     await ctx.send(mensagem_bonitinha)
-    await ctx.send(f'1 Euro está custando {cotacao.euro()} reais :money_with_wings: ')      #euro
+    await ctx.send(f'1 Euro está custando {cotacao.euro()} reais. :money_with_wings: ')      #euro
 
 @client.command()  
 async def libra(ctx):
     await ctx.send(mensagem_bonitinha)
-    await ctx.send(f'1 Libra está custando {cotacao.libra()} reais :money_with_wings: ')     #libra
+    await ctx.send(f'1 Libra está custando {cotacao.libra()} reais. :money_with_wings: ')     #libra
 
 @client.command()  
 async def rublo(ctx):
     await ctx.send(mensagem_bonitinha)
-    await ctx.send(f'1 Rublo Russo está custando {cotacao.rublo()} reais :money_with_wings: ')   #rublo
+    await ctx.send(f'1 Rublo Russo está custando {cotacao.rublo()} reais. :money_with_wings: ')   #rublo
 
 @client.command()  
 async def peso(ctx):
     await ctx.send(mensagem_bonitinha)
-    await ctx.send(f'1 Peso Argentino está custando {cotacao.peso()} reais :money_with_wings: ')    #peso
+    await ctx.send(f'1 Peso Argentino está custando {cotacao.peso()} reais. :money_with_wings: ')    #peso
 
 @client.command()  
 async def bitcoin(ctx):
     await ctx.send(mensagem_bonitinha)
-    await ctx.send(f'1 Bitcoin está custando {cotacao.bitcoin()} reais :money_with_wings: ')     #bitcoin
+    await ctx.send(f'1 Bitcoin está custando {cotacao.bitcoin()} reais. :money_with_wings: ')     #bitcoin
 
 @client.command()  
 async def vbuck(ctx):
     await ctx.send('KKKKKKKKK VBUCK TA CARO PRA CARALHO')     #vbuck
+#------------------------------------------------------------------------------------
 
-# @client.command()  
-# async def (ctx):
-#     await ctx.send('')
+@client.command()
+async def converter(ctx, arg):
+    multiplicacao = (float("{0:.4}".format(dolar_float))*float("{0:.4}".format(arg)))
+    await ctx.send (f'{arg} Dolares valem mais ou menos {multiplicacao} reais.')
 
-
-
-client.run(#token) 
-
-
+#------------------------------------------------------------------------------------------------------
+client.run("#token")
