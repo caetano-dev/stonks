8a2307edf4ac09e9346cd02fd5609e3863da0b9e
Pedro Pires <pcaetanop@gmail.com>
2020-01-31 13:25:21 -0300

    commit

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class Cotacao:
     def __get_cotacao(self, url, regex='^.*nacional" value="([0-9,]+)"'):   #pegar o valor das moedas
         pagina = req.urlopen(url)
         s = pagina.read().decode('utf-8')
-
         m = re.match(regex, s, re.DOTALL)
         if m:
             return float(m.group(1).replace(',', '.'))
@@ -39,8 +38,8 @@ class Cotacao:
 cotacao = Cotacao()
 dolar_float = float(cotacao.dolar())
 #-----------------------------------------------------------------------------------------------------------------------
-client = commands.Bot(command_prefix = ".")   #prefixo do bot 
-client.remove_command('help')      #remove o comando help para fazer um personalizado
+client = commands.Bot(command_prefix = ".")   #prefix
+client.remove_command('help')
 mensagem_bonitinha = ('Analisando minha bolsa de valores:chart_with_upwards_trend:...Por favor aguarde.') #mensagem antes dos valores
 
 print('software online')
@@ -52,7 +51,7 @@ async def on_ready():
 #-----------------------------------------    #comandos
 @client.command(pass_context = True)
 async def help(ctx):
-    author = ctx.message.author        
+    author = ctx.message.author
     embed = discord.Embed(
         colour = discord.Colour.blue()
     )
@@ -68,49 +67,56 @@ async def help(ctx):
     embed.add_field(name=".imposto", value='Fala umas verdades.', inline = False)
     await ctx.send(author, embed=embed)
     
-@client.command()                          
+@client.command()
 async def dolar(ctx):
-    msg = await ctx.send(mensagem_bonitinha)   
-    sleep(1)  
+    msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Dolar está custando {cotacao.dolar()} Reais. :money_with_wings: '))    #dolar
 
 
-@client.command()                     
+@client.command()
 async def euro(ctx):
     msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Euro está custando {cotacao.euro()} Reais. :money_with_wings: '))      #euro
 
-@client.command()  
+@client.command()
 async def libra(ctx):
     msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Libra está custando {cotacao.libra()} Reais. :money_with_wings: '))     #libra
 
-@client.command()  
+@client.command()
 async def rublo(ctx):
     msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Rublo Russo está custando {cotacao.rublo()} Reais. :money_with_wings: '))   #rublo
 
-@client.command()  
+@client.command()
 async def peso(ctx):
     msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Peso Argentino está custando {cotacao.peso()} Reais. :money_with_wings: '))    #peso
 
-@client.command()  
+@client.command()
 async def bitcoin(ctx):
     msg = await ctx.send(mensagem_bonitinha)
+    sleep(1)
     await msg.edit(content=(f'1 Bitcoin está custando {cotacao.bitcoin()} Reais. :money_with_wings: '))     #bitcoin
 
-@client.command()  
+@client.command()
 async def vbuck(ctx):
-    await ctx.send('KKKKKKKKK VBUCK TA CARO DEMAIS MANO.')                                         #vbuck
+    await ctx.send(mensagem_bonitinha)
+    sleep(1)
+    await ctx.send('KKKKKKKKK VBUCK TA CARO DEMAIS MANO. VAI DAR NAO.')                                         #vbuck
 
-@client.command()                
-async def imposto(ctx):                      
+@client.command()
+async def imposto(ctx):
     msg = await ctx.send('IMPOSTO')
     sleep(1)
     await msg.edit(content=('É'))
     sleep(1)
-    await msg.edit(content=('ROBÔ :robot:'))
+    await msg.edit(content=("ROBÔ :robot:"))
 
 @client.command()
 async def stonks(ctx):
@@ -121,10 +127,10 @@ async def stonks(ctx):
 @client.command()
 async def converter(ctx, arg):
     try:
-        multiplicacao = (float("{}".format(dolar_float))*float("{}".format(arg)))        #converter{0:.4}
+        multiplicacao = (float("{}".format(dolar_float))*float("{}".format(arg)))        #convert
         await ctx.send (f'{arg} Dolares valem mais ou menos {trunc(multiplicacao)} Reais.')
-    except: 
-        await ctx.send("Formato não suportado. Tente usar ponto ao invés de vírgula. ")
+    except:
+        await ctx.send("Formato não suportado. Tente usar ponto ao invés de vírgula.")
 #------------------------------------------------------------------------------------------------------
 
-# client.run(#your token)
+# client.run("token")
